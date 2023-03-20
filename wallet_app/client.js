77d618e627437cf00569f21f5b3fd3557791c913
murray22 <murray22@localhost>
2023-03-20 15:55:20 -0600

    Wallet app client

---
+++ wallet_app/client.js
@@ -0,0 +1,44 @@
+//import MyAlgoConnect from '@randlabs/myalgo-connect';
+//import algosdk from 'algosdk';
+const algosdk = require("algosdk");
+const MyAlgoConnect = require('@randlabs/myalgo-connect');
+const api_token0 = 'b2c3d79ef5621b161ff6bb13ed4d4cf58cc0f0bfac2494db7a1e06cb199d135a';
+const server0 = 'http://127.0.0.1';
+const port0 = 8080;
+
+const api_token1 = '9c16e8dbd211a5397089c5f05d07d6d003c45349125983b38779549d5dce031e';
+const server1 = 'http://127.0.0.1';
+const port1 = 36041;
+
+
+const algodClientNode0 = new algosdk.Algodv2(api_token0, server0, port0);
+const algodClientNode1 = new algosdk.Algodv2(api_token1, server1, port1);
+(async () => {
+	  console.log(await algodClientNode0.status().do());
+	})().catch((e) => {
+		console.log(e);
+	}
+);
+(async () => {
+const params = await algodClientNode1.getTransactionParams().do();
+const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+	  Params: {
+		        ...params,
+		    },
+	  from: 'PNBE4HIZJSAUHCVCTOH7X6YHDNH2DXH6EF5SZCDSMQNBISQ4WI7GZ6HMEE',
+	  to: 'LYMCGSOD6IB3GFCMNRQM7NHEJWHW3OW57VP5DJI4UA4LJWTL6JA5FCQEKM',
+	  amount: 1000,
+	  note: 'TEST'
+});
+	const myAlgoConnect = new MyAlgoConnect();
+	const accountsSharedByUser = await myAlgoConnect.connect()
+	const [ signedTxn ] = await myAlgoConnect.signTxns([{
+		  txn: Buffer.from(txn.toByte()).toString('base64')
+	}]);
+const txBytes = Buffer.from(signedTxn, 'base64')
+const response = await algodClient.sendRawTransaction(txBytes).do();
+console.log('RESPONSE: ', response)
+	// for each node 0..n
+})().catch((e) => {
+	  console.log(e);
+});
